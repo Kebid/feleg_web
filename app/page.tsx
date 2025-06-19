99c06fab011903c35ac0ec22fb6757d6990877c1
Kebid <103946194+Kebid@users.noreply.github.com>
2025-06-19 22:51:40 +0300

    UI/UX polish

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ import { motion } from "framer-motion";
 export default function Home() {
   const router = useRouter();
   useEffect(() => {
-    router.replace("/login");
+    // router.replace("/login");
   }, [router]);
 
   useEffect(() => {
